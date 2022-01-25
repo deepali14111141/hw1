1aa95b6bed8722230f8b0d0e53ac4992274bc88c
deepali14111141 <Deepali.Worlikar@kellogg.northwestern.edu>
2022-01-25 04:42:32 +0000

    Task 2-2

--- kmdb.sql
+++ kmdb.sql
@@ -161,6 +161,94 @@ VALUES (
     "PG-13",
     1
 );
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Christian",
+    "Bale"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Michael",
+    "Cain"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Liam",
+    "Neeson"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Katie",
+    "Holmes"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Gary",
+    "Oldman"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Heath",
+    "Ledger"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Aaron",
+    "Eckhart"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Maggie",
+    "Gyllenhaal"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Tom",
+    "Hardy"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Joseph",
+    "Gordon-Levitt"
+);
+INSERT INTO actors (
+    first_name,
+    last_name
+)
+VALUES (
+    "Anne",
+    "Hathway"
+);
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
